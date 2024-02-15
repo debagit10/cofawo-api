78bb9bf020e88a53ebb2fb8e6947ccc148517a33
Josiah Elias <josiahelias11@gmail.com>
2024-02-15 22:21:07 +0000

    fix: body variable naming convention

--- backend/controllers/waterController.js
+++ backend/controllers/waterController.js
@@ -2,12 +2,13 @@ const { v4: uuidv4 } = require("uuid");
 const pool = require("../db");
 
 const addWaterData = async (req, res) => {
-  const { gender, hostel, disease, action, domesticUse, suggestion } = req.body;
+  const { gender, hostel, disease, action, domestic_use, suggestion } =
+    req.body;
   const id = uuidv4();
   try {
     const water = await pool.query(
       "INSERT INTO water(id,gender,hostel,disease,action,domestic_use,suggestion) VALUES($1,$2,$3,$4,$5,$6,$7)",
-      [id, gender, hostel, disease, action, domesticUse, suggestion]
+      [id, gender, hostel, disease, action, domestic_use, suggestion]
     );
 
     if (water.command == "INSERT") {
